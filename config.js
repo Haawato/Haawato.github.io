e2d7ff7a7f60406ee235b7d207f4a30b22071d14
Haawato <qhuy30122005@gmail.com>
2024-01-29 21:30:10 +0700

    first project

---
+++ config.js
@@ -0,0 +1,20 @@
+//====Nhập Thông Tin Vào Đây==============
+//Link Ảnh Background
+ImgLink_Background = "./IMG/BACKGROUND.jpg"
+
+//Link Ảnh Avatar
+ImgLink_Avatar = "./IMG/AVTPRO5.jpg"
+
+//Nickname
+Nickname_Profile = "Haawato"
+
+//Tên Thật
+RealName_Profile = "Ngô Huyyy"
+
+//Các Link Của Bạn
+LinkInformation = [
+    Facebook_LinkProfile = "https://www.facebook.com/ngohyyyyyy?locale=vi_VN",
+    Discord_LinkProfile = "nhuoctuyet",
+    TikTok_LinkProfile = "https://www.tiktok.com/@ng.yuh",
+    Instagram_LinkProfile = "https://www.instagram.com/ng_yuh05/"
+]
